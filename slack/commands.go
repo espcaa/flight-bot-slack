d8da0a65cd9074ca2e494acab02e67ad1e1a4e7d
alice <122629939+espcaa@users.noreply.github.com>
2025-10-30 21:22:18 +0100

    better handler

--- slack/commands.go
+++ slack/commands.go
@@ -4,7 +4,7 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"net/http"
 )
 
@@ -32,6 +32,7 @@ func AddFlightHandler(w http.ResponseWriter, r *http.Request, slackToken string)
 
 	commandText := r.FormValue("text")
 	channelID := r.FormValue("channel_id")
+	webhookURL := r.FormValue("response_url")
 
 	var flightNumber string
 	var date string
@@ -54,7 +55,7 @@ func AddFlightHandler(w http.ResponseWriter, r *http.Request, slackToken string)
 
 	message := fmt.Sprintf("You requested tracking for flight *%s* on date *%s*.", flightNumber, date)
 
-	err = SendSlackMessage(channelID, message, slackToken)
+	err = answerWebhook(webhookURL, message)
 	if err != nil {
 		fmt.Println("Error sending Slack message:", err)
 		return
@@ -62,34 +63,24 @@ func AddFlightHandler(w http.ResponseWriter, r *http.Request, slackToken string)
 
 }
 
-func SendSlackMessage(channelID string, message string, slack_token string) error {
-	slackApiURL := "https://slack.com/api/chat.postMessage"
-	requestBody, err := json.Marshal(map[string]string{
-		"channel": channelID,
-		"text":    message,
-	})
+func answerWebhook(webhookURL string, message string) error {
+	payload := map[string]string{
+		"text": message,
+	}
+	payloadBytes, err := json.Marshal(payload)
 	if err != nil {
 		return err
 	}
 
-	req, err := http.NewRequest("POST", slackApiURL, bytes.NewBuffer(requestBody))
-	if err != nil {
-		return err
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+slack_token)
-
-	client := &http.Client{}
-	resp, err := client.Do(req)
+	resp, err := http.Post(webhookURL, "application/json", bytes.NewBuffer(payloadBytes))
 	if err != nil {
 		return err
 	}
 	defer resp.Body.Close()
 
-	if resp.StatusCode != http.StatusOK {
-		body, _ := ioutil.ReadAll(resp.Body)
-		return fmt.Errorf("Slack API error: %s", string(body))
+	_, err = io.ReadAll(resp.Body)
+	if err != nil {
+		return err
 	}
 
 	return nil
