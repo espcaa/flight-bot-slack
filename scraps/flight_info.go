69f694446117f95c20a302938f1112f271dc1349
alice <122629939+espcaa@users.noreply.github.com>
2025-10-31 15:18:24 +0100

    okay timeout ig

--- scraps/flight_info.go
+++ scraps/flight_info.go
@@ -28,7 +28,7 @@ func GetFlightInfo(flightNumber string) (structs.FlightDataWrapper, error) {
 	// create a http client with timeout
 
 	client := &http.Client{
-		Timeout: 10 * time.Second,
+		Timeout: 100 * time.Second,
 	}
 	headers := map[string]string{
 		"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36",
