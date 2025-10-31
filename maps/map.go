3bace6480e25e6c5aacc1f4a670ccef1652ad95f
alice <122629939+espcaa@users.noreply.github.com>
2025-10-31 15:41:46 +0100

    better map

--- maps/map.go
+++ maps/map.go
@@ -1,10 +1,11 @@
 package maps
 
 import (
-	"fmt"
-	"image/color"
-
 	structs "flight-tracker-slack/types"
+	"fmt"
+	"image"
+	"image/color"
+	"os"
 
 	sm "github.com/flopp/go-staticmaps"
 	"github.com/fogleman/gg"
@@ -12,20 +13,36 @@ import (
 	"github.com/google/uuid"
 )
 
-func GenerateAircraftMap(lat float64, lon float64, tracks []structs.TrackPoint) (string, error) {
+func GenerateAircraftMap(lat float64, lon float64, tracks []structs.TrackPoint, heading int) (string, error) {
 	ctx := sm.NewContext()
 	ctx.SetSize(400, 300)
 	ctx.SetZoom(2)
 
 	aircraftPos := s2.LatLngFromDegrees(lat, lon)
-	ctx.AddObject(sm.NewMarker(aircraftPos, color.RGBA{0, 255, 0, 255}, 16.0))
+
+	osfile, err := os.Open("assets/plane.png")
+	if err != nil {
+		return "", fmt.Errorf("failed to open plane image: %w", err)
+	}
+	defer osfile.Close()
+
+	imga, _, err := image.Decode(osfile)
+	if err != nil {
+		return "", fmt.Errorf("failed to decode plane image: %w", err)
+	}
+
+	dc := gg.NewContextForImage(imga)
+	dc.RotateAbout(gg.Radians(float64(heading)), float64(imga.Bounds().Dx()/2), float64(imga.Bounds().Dy()/2))
+	rotated := dc.Image()
+
+	ctx.AddObject(sm.NewImageMarker(aircraftPos, rotated, 50, 50))
 
 	if len(tracks) > 1 {
 		pathPositions := make([]s2.LatLng, 0, len(tracks))
 		for _, t := range tracks {
-			pathPositions = append(pathPositions, s2.LatLngFromDegrees(t.Coord[0], t.Coord[1]))
+			pathPositions = append(pathPositions, s2.LatLngFromDegrees(t.Coord[1], t.Coord[0]))
 		}
-		ctx.AddObject(sm.NewPath(pathPositions, color.RGBA{0, 0, 255, 255}, 3.0))
+		ctx.AddObject(sm.NewPath(pathPositions, color.RGBA{221, 55, 255, 255}, 1.0))
 	}
 
 	img, err := ctx.Render()
@@ -34,7 +51,6 @@ func GenerateAircraftMap(lat float64, lon float64, tracks []structs.TrackPoint) 
 	}
 
 	fileName := fmt.Sprintf("aircraft-map-%s.png", uuid.New().String())
-
 	if err := gg.SavePNG(fileName, img); err != nil {
 		return "", fmt.Errorf("failed to save map PNG: %w", err)
 	}
